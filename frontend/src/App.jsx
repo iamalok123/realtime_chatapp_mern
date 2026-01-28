796f311e7fb9ed31abb64620fa2355db9389217a
Alok <alokhotta10@gmail.com>
2026-01-28 13:34:03 +0530

    signup and login feature and page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore'
+import Loader from './components/Loader'
+import { Toaster } from 'react-hot-toast'
+
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []); // Empty dependency - checkAuth is stable from Zustand
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <Loader />
+    )
+  }
+
   return (
-    <div className="text-2xl text-red-400 flex justify-center items-center h-screen">
-      Hello world
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+
+      </Routes>
+
+      <Toaster />
     </div>
   )
 }
