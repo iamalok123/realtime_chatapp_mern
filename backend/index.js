9ec973689f14f88bedbcffb9ce6152d5931037d7
Alok <alokhotta10@gmail.com>
2026-01-29 23:42:05 +0530

    build home page

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 const PORT = process.env.PORT || 4000;
